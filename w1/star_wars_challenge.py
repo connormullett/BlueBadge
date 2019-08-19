e35089af948e4cff8c95aa56dded6f1a6568c996
Connor Mullett <connormullett@gmail.com>
2019-08-19 13:39:03 -0400

    finished w1d1

--- w1/star_wars_challenge.py
+++ w1/star_wars_challenge.py
@@ -1,5 +1,17 @@
 
 import requests
 
-species = requests.get('http://swapi.co')
+species = requests.get('https://swapi.co/api/species')
+species_count = species.json()['count']
+
+planets = requests.get('https://swapi.co/api/planets')
+planets_count = planets.json()['count']
+
+vehicles = requests.get('https://swapi.co/api/vehicles')
+vehicles_count = vehicles.json()['count']
+
+
+print(f'planets  : {planets_count}')
+print(f'species  : {species_count}')
+print(f'vehicles : {vehicles_count}')
 
